34e92fd08afa35df38ef72912c963477a614f048
Sander Houttekier <sander.houttekier@gmail.com>
2018-02-19 23:36:22 +0100

    fix arrow keys

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,14 +1,92 @@
+var MOVES = {
+	LEFT: "left",
+	RIGHT: "right"
+}
+var KEYCODES = {
+	LEFTARROW: 37,
+	RIGHTARROW: 39
+}
+
 chrome.extension.sendMessage({}, function(response) {
 
 	var readyStateCheckInterval = setInterval(function() {
 		if (document.readyState === "complete") {
 			clearInterval(readyStateCheckInterval);
 
+			// make left and right keys work just like up and down keys...
+			document.onkeydown = checkKey;
+
 			GuardCompleteButton();
-
-
 		}
 	}, 10);
+
+
+	function checkKey(e) {
+		e = e || window.event;
+		var inputElement = e.target;
+
+		var isNormalHoursInput = inputElement && inputElement.id === 'normalHoursInput';
+
+		if (!isNormalHoursInput) {
+			return;
+		}
+
+		if (e.keyCode === KEYCODES.LEFTARROW) {
+			move(MOVES.LEFT, inputElement);
+		}
+		else if (e.keyCode === KEYCODES.RIGHTARROW) {
+			move(MOVES.RIGHT, inputElement);
+		}
+	}
+
+	function triggerMove(direction, input) {
+		var activeRowElements = document.querySelectorAll('.active-row-level.hours-input');
+
+		var activeIndex;
+		for (var i = 0; i <= activeRowElements.length -1; i++) {
+			var item = activeRowElements[i];
+			var isActive = item.classList.contains('active');
+			if (isActive) {
+				activeIndex = i;
+			}
+		}
+
+		var newIndex;
+		if (activeIndex !== undefined) {
+			if (direction === MOVES.LEFT) {
+				newIndex = activeIndex - 1;
+			} else {
+				newIndex = activeIndex + 1;
+			}
+		}
+
+		if (newIndex === undefined) {
+			return
+		}
+
+		var newActiveItem = activeRowElements[newIndex];
+		if (newActiveItem) {
+				newActiveItem.click();
+		}
+	}
+
+	function move(direction, input) {
+		var value = input.value;
+		var selectionStart = input.selectionStart;
+
+		switch(direction) {
+			case MOVES.LEFT:
+				if (selectionStart === 0) {
+					triggerMove(direction, input);
+				}
+			break;
+			case MOVES.RIGHT:
+				if (selectionStart === value.length) {
+					triggerMove(direction, input);
+				}
+			break;
+		}
+	}
 
 	function setActivity(el, active) {
 		return el.classList.toggle('active', !!active);
