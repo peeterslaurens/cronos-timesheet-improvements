71db8bade027096d31ce9dee37eab1d7420e9530
Sander Houttekier <sander.houttekier@gmail.com>
2018-03-07 13:00:01 +0100

    initial try to hook into XHR

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,10 +4,30 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+// chrome.extension.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//   	chrome.pageAction.show(sender.tab.id);
+//     sendResponse();
+//   });
+
+var counter = 0;
+chrome.browserAction.onClicked.addListener(function (tab) {
+    counter++;
+    if (counter == 5) {
+        alert("Hey !!! You have clicked five times");
+    }
+});
+
+console.log(chrome.extension.onRequest.addListener);
+console.log(chrome.extension.onMessage.addListener);
+chrome.extension.onRequest.addListener(function(request) {
+  console.log(request);
+
+	if (request.command !== 'sendToConsole')
+		return;
+
+	chrome.tabs.executeScript(request.tabId, {
+		code: "'" + request.args + "';",
+	});
+});
